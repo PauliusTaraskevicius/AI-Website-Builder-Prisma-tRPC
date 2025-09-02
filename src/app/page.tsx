2dffee2412dd0ba0274bbb7bada0429200bf97a2
PauliusTaraskevicius <fixas1029@gmail.com>
2025-09-02 12:01:49 +0300

    04-background jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
 export default function Home() {
-  return (
-    <div className="text-bold text-rose-500">
-      <p>Hello world</p>
-    </div>
-  );
+  return <div className="p-4 max-w-7xl mx-auto"></div>;
 }
