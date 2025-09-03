dcbeb986dfea3ad15e3e8284346aabdd6135b995
PauliusTaraskevicius <fixas1029@gmail.com>
2025-09-03 11:08:57 +0300

    06-e2b sandbox

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,21 +1,33 @@
 import { inngest } from "./client";
 import { openai, createAgent } from "@inngest/agent-kit";
+import { Sandbox } from "@e2b/code-interpreter";
+import { getSandbox } from "./utils";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event }) => {
+  async ({ event, step }) => {
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+      const sandbox = await Sandbox.create("ai-website-builder-1234");
+      return sandbox.sandboxId;
+    });
+
     const summarizer = createAgent({
       name: "summarizer",
       system: "You are an expert summarizer.  You summarize in 2 words.",
-      model: openai({ model: "gpt-4o", apiKey: process.env.OPEN_AI_API_KEY }),
+      model: openai({ model: "gpt-4o", apiKey: process.env.OPENAI_API_KEY }),
     });
 
     const { output } = await summarizer.run(
       `Summarize the following text: ${event.data.value}`
     );
-    console.log(output);
-    // [{ role: 'assistant', content: 'function removeUnecessaryWhitespace(...' }]
-    return { output };
+
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await getSandbox(sandboxId);
+      const host = sandbox.getHost(3000);
+      return `https://${host}`;
+    });
+
+    return { output, sandboxUrl };
   }
 );
