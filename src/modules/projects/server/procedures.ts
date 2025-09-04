f5b49d942302751f9db3456ca3f394864c856925
PauliusTaraskevicius <fixas1029@gmail.com>
2025-09-04 11:56:19 +0300

    09-projects

--- src/modules/projects/server/procedures.ts
+++ src/modules/projects/server/procedures.ts
@@ -2,8 +2,9 @@ import { inngest } from "@/inngest/client";
 import prisma from "@/lib/db";
 import { baseProcedure, createTRPCRouter } from "@/trpc/init";
 import { z } from "zod";
+import { generateSlug } from "random-word-slugs";
 
-export const messagesRouter = createTRPCRouter({
+export const projectsRouter = createTRPCRouter({
   create: baseProcedure
     .input(
       z.object({
@@ -11,16 +12,21 @@ export const messagesRouter = createTRPCRouter({
           .string()
           .min(1, "Value is required")
           .max(10000, "Value is too long"),
-        projectId: z.string().min(1, "Project ID is required"),
       })
     )
     .mutation(async ({ input }) => {
-      const newMessage = await prisma.message.create({
+      const createdProject = await prisma.project.create({
         data: {
-          projectId: input.projectId,
-          content: input.value,
-          role: "USER",
-          type: "RESULT",
+          name: generateSlug(2, {
+            format: "kebab",
+          }),
+          messages: {
+            create: {
+              content: input.value,
+              role: "USER",
+              type: "RESULT",
+            },
+          },
         },
       });
 
@@ -28,22 +34,19 @@ export const messagesRouter = createTRPCRouter({
         name: "code-agent/run",
         data: {
           value: input.value,
-          projectId: input.projectId,
+          projectId: createdProject.id,
         },
       });
 
-      return newMessage;
+      return createdProject;
     }),
   getMany: baseProcedure.query(async () => {
-    const messages = await prisma.message.findMany({
+    const projects = await prisma.project.findMany({
       orderBy: {
         upodatedAt: "asc",
       },
-      include: {
-        fragment: true,
-      },
     });
 
-    return messages;
+    return projects;
   }),
 });
