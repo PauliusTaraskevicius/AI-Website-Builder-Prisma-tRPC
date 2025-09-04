e444944e8ed7b4cd57b280ce6c62e108d7b569f4
PauliusTaraskevicius <fixas1029@gmail.com>
2025-09-04 11:29:44 +0300

    08-messages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,35 @@
-'use client'
+"use client";
 
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { useTRPC } from "@/trpc/client";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { useState } from "react";
+import { toast } from "sonner";
 
 export default function Home() {
   const [value, setValue] = useState("");
 
+  const trpc = useTRPC();
+  const { data: messages } = useQuery(trpc.messages.getMany.queryOptions());
+  const createdMessage = useMutation(
+    trpc.messages.create.mutationOptions({
+      onSuccess: () => {
+        toast.success("Message created");
+      },
+    })
+  );
+
   return (
     <div className="p-4 max-w-7xl mx-auto">
       <Input value={value} onChange={(e) => setValue(e.target.value)} />
+      <Button
+        disabled={createdMessage.isPending}
+        onClick={() => createdMessage.mutate({ value })}
+      >
+        Create message
+      </Button>
+      {JSON.stringify(messages, null, 2)}
     </div>
   );
 }
